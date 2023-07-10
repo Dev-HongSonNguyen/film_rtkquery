f583e189d8c76240e0f0bbe096692a326e413819
nguyenhongson <nguyenhongson20433@gmail.com>
2023-07-11 02:08:55 +0700

    init-project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return <div className="App">This is my Film App</div>;
+}
+
+export default App;
